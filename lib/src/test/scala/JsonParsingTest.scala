9870f86488a80270a2240c14200a461767b351f7
jackhopner <jack@movio.co>
2016-07-26 15:06:40 +1200

    Add new generated classes and json test

---
+++ lib/src/test/scala/JsonParsingTest.scala
@@ -0,0 +1,45 @@
+package movio.cinema.apidoc.generator.reference
+
+import scala.util.Try
+import scala.util.Success
+
+import org.joda.time.DateTime
+import org.joda.time.format._
+import org.mockito.Matchers.any
+import org.mockito.Matchers.{ eq ⇒ is }
+import play.api.libs.json._
+
+import com.typesafe.config.ConfigFactory
+
+import movio.testtools.MovioSpec
+
+
+class JsonParsingTest extends MovioSpec {
+  import movio.cinema.apidoc.generator.reference.v0.models._
+  import movio.cinema.apidoc.generator.reference.v0.models.json._
+
+  describe("datetime validation") {
+    it("should throw an jsresultexception if the datetime has no tz offset") {
+      intercept[JsResultException] {
+        val json = """{"id":"1","name":"John","lastActiveTime":"2016-12-24T15:36:54","addresses":[]}"""
+
+        Json.parse(json).as[Person]
+      }
+    }
+    it("should UTC as tz offset") {
+      val json = """{"id":"1","name":"John","lastActiveTime":"2016-12-24T15:36:54Z","addresses":[]}"""
+
+      Json.parse(json).as[Person]
+    }
+    it("should support tz offsets") {
+      val json = """{"id":"1","name":"John","lastActiveTime":"2016-12-24T15:36:54+0400","addresses":[]}"""
+
+      Json.parse(json).as[Person]
+    }
+    it("should support milliseconds") {
+      val json = """{"id":"1","name":"John","lastActiveTime":"2016-12-24T15:36:54.932+0400","addresses":[]}"""
+
+      Json.parse(json).as[Person]
+    }
+  }
+}
